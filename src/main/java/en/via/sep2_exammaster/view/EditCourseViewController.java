8de7306c07c45d5c4a9aad424c7fb467df4425f6
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2024-05-13 10:24:21 +0200

    comitting for you maria

---
+++ src/main/java/en/via/sep2_exammaster/view/EditCourseViewController.java
@@ -0,0 +1,110 @@
+package en.via.sep2_exammaster.view;
+
+import en.via.sep2_exammaster.shared.Student;
+import en.via.sep2_exammaster.viewmodel.CourseInfoViewModel;
+import en.via.sep2_exammaster.viewmodel.EditCourseViewModel;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class EditCourseViewController implements PropertyChangeListener {
+
+  @FXML public TextField codeField;
+  @FXML public TextField semesterField;
+  @FXML public TextField titleField;
+  @FXML public TextArea descriptionArea;
+  @FXML public TextField additonalTeacherField;
+  @FXML public TextField studentField;
+  @FXML public ListView<Student> studentsList;
+  @FXML public Button removeButton;
+
+  private ViewHandler viewHandler;
+  private EditCourseViewModel viewModel;
+  private Region root;
+
+  @FXML void onClick(MouseEvent event) {
+
+  }
+
+  @FXML void onRemove(ActionEvent event) {
+
+  }
+
+  @FXML void onSave(ActionEvent event) {
+
+  }
+
+  public void init(ViewHandler viewHandler, EditCourseViewModel editCourseViewModel, Region root) {
+    this.viewHandler = viewHandler;
+    this.viewModel = editCourseViewModel;
+    this.root = root;
+
+    viewModel.bindCode(codeField.textProperty());
+    viewModel.bindSemester(semesterField.textProperty());
+    viewModel.bindTitle(titleField.textProperty());
+    viewModel.bindDescription(descriptionArea.textProperty());
+    viewModel.bindAdditionalTeacher(additonalTeacherField.textProperty());
+    viewModel.bindStudent(studentField.textProperty());
+    viewModel.bindStudents(studentsList.itemsProperty());
+
+    removeButton.setDisable(true);
+  }
+
+  public Region getRoot() {
+    return root;
+  }
+
+  public void reset() {
+    removeButton.setDisable(true);
+    viewModel.reset();
+  }
+
+  public void showError(String message){
+    Alert alert = new Alert(Alert.AlertType.WARNING, message);
+    alert.setHeaderText(null);
+    alert.showAndWait();
+  }
+
+  @Override public void propertyChange(PropertyChangeEvent evt) {
+    switch (evt.getPropertyName()){
+      case "course create success" ->
+          Platform.runLater(() -> {
+            viewModel.removeListener(this);
+            viewHandler.openView(ViewFactory.MY_COURSES);
+          });
+      case "course create fail" ->
+          Platform.runLater(() -> {
+            showError("A course with this course code already exists. A different code must be used.");
+          });
+      case "semester error" ->
+          Platform.runLater(() -> {
+            showError("The \"Semester\" field only accepts numbers.");
+          });
+      case "student parsing error" ->
+          Platform.runLater(() -> {
+            showError("The \"Student\" field only accepts numbers.");
+          });
+      case "student adding error" ->
+          Platform.runLater(() -> {
+            showError("The provided Student ID belongs to a student already enrolled in semester.");
+          });
+      case "student not found" ->
+          Platform.runLater(() -> {
+            showError("Student ID is incorrect.");
+          });
+      case "teacher not found" ->
+          Platform.runLater(() -> {
+            showError("Teacher initials are incorrect.");
+          });
+    }
+
+  }
+
+}
+
