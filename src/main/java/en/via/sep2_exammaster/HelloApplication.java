f3868a72ae52aa60e9851b9cb83924be65e11d1f
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2024-05-28 11:34:54 +0200

    fixed testing?

--- src/main/java/en/via/sep2_exammaster/HelloApplication.java
+++ src/main/java/en/via/sep2_exammaster/HelloApplication.java
@@ -25,6 +25,15 @@ public class HelloApplication extends Application
 {
   static Model client;
 
+  /**
+   * Starts the client-side application.
+   *
+   * @param primaryStage the primary stage for this application, onto which
+   * the application scene can be set.
+   * Applications may create other stages, if needed, but they will not be
+   * primary stages.
+   * @throws Exception if any exception occurs while starting the application
+   */
   @Override public void start(Stage primaryStage) throws Exception {
     Registry registry = LocateRegistry.getRegistry(1099);
     ServerConnector serverConnector = (ServerConnector) registry.lookup("server");
@@ -34,10 +43,19 @@ public class HelloApplication extends Application
     viewHandler.start(primaryStage);
   }
 
+  /**
+   * Code executed when the application is turned off.
+   *
+   * @throws Exception if any exception occurs while closing
+   */
   @Override public void stop() throws Exception {
     client.close();
   }
 
+  /**
+   * main class duh
+   * @param args we dont really know
+   */
   public static void main(String[] args) {
     launch();
   }
